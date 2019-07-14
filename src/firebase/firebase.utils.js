a4419b6c74688f921fa37fd77fe29ed82ded79ab
austinjblake <46485261+austinjblake@users.noreply.github.com>
2019-07-14 09:11:06 -0500

    checking firebase and creating new user doc if null. getting user info from firebase and setting into app state

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,6 +10,31 @@ const config = {
   storageBucket: "",
   messagingSenderId: "215141141383",
   appId: "1:215141141383:web:53894eb08d719ef2"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (err) {
+      console.log('error creating user', err.message);
+    }
+  }
+
+  return userRef;
 }
 
 firebase.initializeApp(config);
